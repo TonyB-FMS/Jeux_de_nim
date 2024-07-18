61306e941085f38ee2be95ca78ab728be96d3775
Tony BORTOLOZZO <tony.bortolozzo@fms-ea.com>
2024-07-18 11:51:00 +0200

    modification du fichier pour l'exercice, ajout de la fonction 'get_player_move' terminée

--- nim_games.py
+++ nim_games.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Jeux de nim
 variante simple et de Marienbad
 """
 
-print("jeux de nim")
+
+def get_player_move(player, remaining_turns):
+    """
+    Ask the player the number of matches to remove and validates.
+
+    Args :
+        player : actual player name.
+        remaining_turns : number of matches still in game.
+
+    Returns :
+        int : number of matches to remove.
+    """
+    while True:
+        try:
+            move = int(input(f"{player}, combien d'allumettes voulez-vous enlever (1-4) ?"))
+            if 1 <= move <= 4 and move <= remaining_turns:
+                return move
+            else:
+                print(f"Entrez un nombre entre 1 et 4 (pas plus que ce qu'il reste: {remaining_turns}")
+        except ValueError:
+            print("Invalide, entrez un nombre.")
